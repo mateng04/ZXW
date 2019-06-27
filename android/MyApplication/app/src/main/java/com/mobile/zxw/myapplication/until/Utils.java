405de0af6bf16ff2b40cc4ecece1fbfb9b77b836
mateng04 <307003873@qq.com>
2019-06-27 16:18:07 +0800

    MT
    注册电话号码增加4和9

--- android/MyApplication/app/src/main/java/com/mobile/zxw/myapplication/until/Utils.java
+++ android/MyApplication/app/src/main/java/com/mobile/zxw/myapplication/until/Utils.java
@@ -66,7 +66,7 @@ public class Utils {
     }
 
     public static boolean isMobileNO(String mobiles) {
-        String telRegex = "[1][3578]\\d{9}";
+        String telRegex = "[1][3456789]\\d{9}";
         // "[1]"代表第1位为数字1，"[3578]"代表第二位可以为3、5、8中的一个，"\\d{9}"代表后面是可以是0～9的数字，有9位。
         if (TextUtils.isEmpty(mobiles)) {
             return false;
